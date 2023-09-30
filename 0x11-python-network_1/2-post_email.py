b79947574c701cc23a3f105ce13fd7532f74e916
izebhigie <dizebhigie@gmail.com>
2023-09-30 06:00:51 +0100

    network 1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,8 +10,8 @@ import urllib.parse
 if __name__ == "__main__":
 
     url = sys.argv[1]
-    value = {'email': sys.argv[2]}
-    parameter = urllib.parse.urlencode(val).encode('ascii')
+    val = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(val).encode('ascii')
 
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
