9a889a40c53074bd5987cea184cc73d594bf8942
izebhigie <dizebhigie@gmail.com>
2023-11-02 05:50:10 +0100

    correction

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,6 @@
-$('document').ready(function () {
-  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
-    $('DIV#hello').text(data.hello);
-  });
+i$('document').ready(function () {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr',
+	  function (data) {
+   		 $('DIV#hello').text(data.hello);
+ 	 });
 });
