84cf96a7fe30b8243de920f2bbcbae59fc7aad1d
Daniel Izebhigie <126702487+izebhigie@users.noreply.github.com>
2023-12-03 04:27:20 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-if (process.argv[2] === undefined || (process.argv[3] === undefined)) {
-  console.log(0);
-} else {
-  process.argv.shift();
-  process.argv.shift();
-  const Myarray = process.argv.map(Number).sort((a, b) => a - b).reverse();
-  console.log(Myarray[1]);
-}
