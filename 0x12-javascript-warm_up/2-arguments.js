64cc450089ebda1b0bd3a65cced42d46ec1819e2
izebhigie <dizebhigie@gmail.com>
2023-12-12 04:07:43 +0100

    args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-  console.log('No argument');
-}
-if (process.argv.length === 3) {
-  console.log('Argument found');
-}
-if (process.argv.length > 3) {
-  console.log('Arguments found');
-}
+const { argv } = require('process');
+if (argv.length === 2) { 
+  console.log('No argument'); } 
+else if (argv.length === 3) { 
+  console.log('Argument found'); } 
+else { 
+  console.log('Arguments found'); }
